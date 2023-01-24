e4f09e1482d49713cd5950067e1b20b974e8f5d0
Rennan Macarini <rennangmacarini@gmail.com>
2023-01-24 17:15:23 -0300

    atualizando

--- src/components/encarte/index.tsx
+++ src/components/encarte/index.tsx
@@ -1,50 +1,50 @@
-import * as React from "react"
-import "keen-slider/keen-slider.min.css"
-import styles from './styles.module.scss'
-import { useKeenSlider, TrackDetails } from "keen-slider/react"
+import * as React from "react";
+import "keen-slider/keen-slider.min.css";
+import styles from "./styles.module.scss";
+import { useKeenSlider, TrackDetails } from "keen-slider/react";
 
 const images = [
   "images/encarte.png",
   "images/encarte.png",
   "images/encarte.png",
-]
+];
 
 export function Encartes() {
-  const [details, setDetails] = React.useState<TrackDetails | null>(null)
+  const [details, setDetails] = React.useState<TrackDetails | null>(null);
 
   const [sliderRef] = useKeenSlider<HTMLDivElement>({
     loop: true,
     detailsChanged(s) {
-      setDetails(s.track.details)
+      setDetails(s.track.details);
     },
     initial: 2,
-  })
+  });
 
   function scaleStyle(idx: number) {
-    if (!details) return {}
-    const slide = details.slides[idx]
-    const scale_size = 0.2
-    const scale = 1 - (scale_size - scale_size * slide.portion)
+    if (!details) return {};
+    const slide = details.slides[idx];
+    const scale_size = 0.2;
+    const scale = 1 - (scale_size - scale_size * slide.portion);
     return {
       transform: `scale(${scale})`,
       WebkitTransform: `scale(${scale})`,
-    }
+    };
   }
 
   return (
-    <div className={styles.global}> 
+    <div className={styles.global}>
       <h1>Ofertas</h1>
       <p></p>
       <h2>Muito mais barato no Ivasko</h2>
-    <div ref={sliderRef} className="keen-slider zoom-out">
-      {images.map((src, idx) => (
-        <div key={idx} className="keen-slider__slide zoom-out__slide">
-          <div style={scaleStyle(idx)}>
-            <img src={src} />
+      <div ref={sliderRef} className="keen-slider zoom-out">
+        {images.map((src, idx) => (
+          <div key={idx} className="keen-slider__slide zoom-out__slide">
+            <div style={scaleStyle(idx)}>
+              <img src={src} />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
