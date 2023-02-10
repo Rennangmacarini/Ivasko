19fbb556b104f2ccc6c7559eeba0329af2852b72
Rennan Macarini <rennangmacarini@gmail.com>
2023-02-10 17:06:19 -0300

    atualizando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,141 +1,30 @@
-import Link from "next/link";
-import { useState } from "react";
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss'
 
 export function Header() {
-  const [openMenu, setOpenMenu] = useState(false);
-
-  /*
-  function teste(){
-  const testeretorno = openMenu ? "./images/close.png" : "./icon/menu.png"
-  return testeretorno
-  }
-  */
-
-  /*
-  function handleIcon(status: boolean){
-    if(status){
-      return "./images/close.png"
-     }
-     else{
-      return "./icon/menu.png"
-     }
-  }
-  */
-
-  const handleOpen = () => {
-    setOpenMenu(!openMenu);
-  };
-
-
-  return (
-    <header>
-      <div className={styles.header}>
-        <img className={styles.logo} src="/images/logo.png" alt="Logo ivasko" />
-        <span onClick={handleOpen}>
-          <img
-            className={styles.buttonMobile}
-            src={openMenu ? "./icon/close.png" : "./icon/menu.png"}
-            alt=""
-          />
-        </span>
-        <div className={styles.menu}>
-          <Link href={"/"}>Início</Link>
-          <Link href={"/about"}>Quem Somos</Link>
-          <Link href={"/stores"}>Lojas</Link>
-          <Link href={"/books"}>Livro de Receitas</Link>
-          <span className={styles.test}>
-            <Link href={"/"}>
-              Área gourmet <img src={"/icon/seta.png"} alt="" />{" "}
-            </Link>
-            <span className={styles.teste}>
-              <Link href={"/butchers"}>Açougue</Link>
-              <Link href={"/bakers"}>Padaria</Link>
-            </span>
-          </span>
-          <Link
-            href="https://app.skeel.com.br/ivasko"
-            target={"_blank"}
-            rel="noreferrer"
-          >
-            Trabalhe Conosco
-          </Link>
-          <Link
-            href="https://meucartao.senff.com.br/ivasko/bem-vindo"
-            target={"_blank"}
-            rel="noreferrer"
-          >
-            CARTÃO IVASKO
-          </Link>
-        </div>
-        <div className={styles.icon}>
-          <div>
-            <Link
-              href="https://www.facebook.com/superivasko/"
-              target={"_blank"}
-              rel="noreferrer"
-            >
-              <img src="/assets/face.png" alt="Logo  facebook" />
-            </Link>
-          </div>
-
-          <div>
-            <Link
-              href="https://www.instagram.com/superivasko/"
-              target={"_blank"}
-              rel="noreferrer"
-            >
-              <img src="/assets/insta.png" alt="Logo Instagram" />
-            </Link>
-          </div>
-
-          <div>
-            <Link
-              href="https://br.linkedin.com/company/grupoivasko"
-              target={"_blank"}
-              rel="noreferrer"
-            >
-              <img src="/assets/linkedin.png" alt="Logo Linkdin" />
-            </Link>
-          </div>
-        </div>
-      </div>
-
-      {openMenu && (
-        <div className={styles.menuContainer}>
-          <div className={styles.menuMobile}>
-            <div className={styles.contentMenu}>
-              <Link href={"/"}>Início</Link>
-              <Link href={"/about"}>Quem Somos</Link>
-              <Link href={"/stores"}>Lojas</Link>
-              <Link href={"/books"}>Livro de Receitas</Link>
-              <span className={styles.test}>
-                <Link href={"/"}>
-                  Área gourmet
-                </Link>
-                <span className={styles.teste}>
-                  <Link href={"/butchers"}>Açougue</Link>
-                  <Link href={"/bakers"}>Padaria</Link>
-                </span>
-              </span>
-              <Link
-                href="https://app.skeel.com.br/ivasko"
-                target={"_blank"}
-                rel="noreferrer"
-              >
-                Trabalhe Conosco
-              </Link>
-              <Link
-                href="https://meucartao.senff.com.br/ivasko/bem-vindo"
-                target={"_blank"}
-                rel="noreferrer"
-              >
-                CARTÃO IVASKO
-              </Link>
+    return (
+        <>
+            <div className={styles.container}>
+                <img className={styles.banner} src={"/img/banner.png"} alt="" />
+                <div className={styles.content}>
+                    <div className={styles.title}>
+                        <h1>POTENCIALIZE SEUS TREINOS</h1>
+                        <h1>EM 5 SEGUNDOS</h1>
+                        <p>Acelere seus resultado através da <strong>Metodologia Gymnamic</strong></p>
+                    </div>
+                    <div className={styles.paragraph}>
+                        <p>Com o seu próprio Planejamento você</p>
+                        <p><strong>Potencializa seus Resultados</strong> em</p>
+                        <p>EMRAGRECIMENTO, GANHO DE MÚSCULOS</p>
+                        <p>e AUMENTO DE FORÇA.</p>
+                    </div>
+                    <div className={styles.button}>
+                        <h2>QUERO PONTENCIALIZAR <br /> MEUS TREINOS</h2>
+                    </div>
+                </div>
+                <div>
+                    <img className={styles.logo} src={"./img/logo.png"} alt="" />
+                </div>
             </div>
-          </div>
-        </div>
-      )}
-    </header>
-  );
+        </>
+    )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,22 @@
 
-import { Banner } from "../components/Banner"
-import { ContentOne } from "../components/ContentOne"
-import { ContentThree } from "../components/ContentThree"
-import { ContentTwo } from "../components/ContentTwo"
-import { Video } from "../components/filme"
-import { Footer } from "../components/footer"
-import { Header } from "../components/Header"
+import {Carousel} from "../components/Carousel";
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+import { Main } from "../components/Main";
+import { Section } from "../components/Section";
+import { SectionOne } from "../components/SectionOne";
+import { SectionTwo } from "../components/SectionTwo";
 
-import { GetStaticProps } from "next"
-import { getPrismicClient } from "../services/prismic"
-import Prismic from "@prismicio/client"
-import { Cookie } from "../components/Cookies"
-import { Ofertas } from "../components/Encarte"
-
-type Posts = {
-  id: string;
-  banner: string;
-  contentone:string;
-  contenttwo: string;
-  contenttwos: string;
-  contentthree:string
+export default function Content() {
+    return (
+        <>
+            <Header />
+            <Section />
+            <Carousel/>
+            <Main/>
+            <SectionOne/>
+            <SectionTwo/>
+            <Footer/>
+        </>
+    )
 }
-
-interface PostProps{
-  posts: Posts[]
-}
-  
-
-export default function Home({posts} : PostProps){
-
-  return(
-    <> 
-    <Header/>
-    <Banner banner={posts} />
-    <ContentOne contentone={posts}/>
-    <ContentTwo contenttwo={posts}/>
-    <ContentThree contentthree={posts}/>
-    <Video/>
-    <Ofertas/>
-    <Cookie/>
-    <Footer/>
-    </>
-  )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient()
-
-  const response = await prismic.query(
-  [Prismic.predicates.at('document.type', 'publication')],
-  {
-    fetch: ['publication.content', 'publication.banner', 'publication.contentone', 'publication.contenttwo', 'publication.contenttwos', 'publication.contentthree'],
-  })
-   
-  const posts = response.results.map(post => {
-    console.log("data", post.data)
-    console.log("post", post)
-    return {
-      id: post.uid,
-      banner:post.data.banner.url,
-      contentone:post.data.contentone.url,
-      contenttwo:post.data.contenttwo.url,
-      contenttwos:post.data.contenttwos.url,
-      contentthree:post.data.contentthree.url
-    };
-  });
-     return{
-      props: {
-          posts
-      }
-     }
-} 
